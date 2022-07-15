4b243119d20ba787abf691ca4f2ffe6babfe288d
ZITOUNI Clement <345567@cus.fr>
2022-07-15 11:57:01 +0200

    cards

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,24 +2,32 @@
 
 <v-container>
 <v-switch v-model="$vuetify.theme.dark" hint="This toggles the global state of the Vuetify theme" inset label="Vuetify Theme Dark" persistent-hint></v-switch>
-<v-toolbar color="green" dense elevation="4" rounded class="my-4"></v-toolbar>
-<v-card  v-for="element in projets" :key="element.id" class=" ma-4">
-  <v-row align="center">
-  <v-col class=" col-3"><v-img  :aspect-ratio="4/3"  max-width = "344"  :src ="element['gmd:identificationInfo']['gmd:MD_DataIdentification']['gmd:graphicOverview']['gmd:MD_BrowseGraphic']['gmd:fileName']['gco:CharacterString']['#text']"></v-img></v-col>
+<v-toolbar color="green darken-3"  dense elevation="4" rounded class="my-4 white--text" fixed></v-toolbar>
+  <v-row justify="center">
+<v-card  v-for="element in projets" :key="element.id" class=" ma-4" max-width="344" >
   <v-col>
-  <v-card-title>{{element["gmd:identificationInfo"]["gmd:MD_DataIdentification"]["gmd:citation"]["gmd:CI_Citation"]["gmd:title"]["gco:CharacterString"]["#text"]}}</v-card-title>
-  <v-card-text>{{element["gmd:identificationInfo"]["gmd:MD_DataIdentification"]["gmd:abstract"]["gco:CharacterString"]["#text"]}}</v-card-text>
-  <v-card-actions>{{element["gmd:identificationInfo"]["gmd:MD_DataIdentification"]["gmd:pointOfContact"][0]["gmd:CI_ResponsibleParty"]["gmd:organisationName"]["gco:CharacterString"]["#text"]}}</v-card-actions>
-  </v-col>
+  <v-row><v-img  :aspect-ratio="16/9"    :src ="element['gmd:identificationInfo']['gmd:MD_DataIdentification']['gmd:graphicOverview']['gmd:MD_BrowseGraphic']['gmd:fileName']['gco:CharacterString']['#text']"></v-img></v-row>
+  <v-row>
+  <v-card-title class="text-center text-break" style="height:150px;" >{{element["gmd:identificationInfo"]["gmd:MD_DataIdentification"]["gmd:citation"]["gmd:CI_Citation"]["gmd:title"]["gco:CharacterString"]["#text"]}}</v-card-title>
+ <v-divider ></v-divider>
+  <p class="text-justify" v-snip:js="3" style="max-height:150px;margin-bottom:75px;margin-left:18px;margin-right:18px;margin-top:18px;">{{element["gmd:identificationInfo"]["gmd:MD_DataIdentification"]["gmd:abstract"]["gco:CharacterString"]["#text"]}}</p>
+
+  <v-btn color="green darken-3" class="white--text"  block style="position:absolute;bottom:0;margin-top:75px;">Consulter</v-btn>
+
   </v-row>
+  </v-col>
 
 </v-card>
+  </v-row>
 </v-container>
 </template>
 
 <script>
 import axios from 'axios'
+
 var convert = require('xml-js');
+
+
 export default{
   
   data () {
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import VueSnip from 'vue-snip'
 
 Vue.config.productionTip = false
 
@@ -9,3 +10,5 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.use(VueSnip)
