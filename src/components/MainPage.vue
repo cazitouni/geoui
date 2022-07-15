de009b89fc5ee47c0bc9c9af65d33eb3e3fa4d9a
ZITOUNI Clement <345567@cus.fr>
2022-07-15 14:40:28 +0200

    ui

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,8 +1,9 @@
 <template>
 
 <v-container>
-<v-switch v-model="$vuetify.theme.dark" hint="This toggles the global state of the Vuetify theme" inset label="Vuetify Theme Dark" persistent-hint></v-switch>
-<v-toolbar color="green darken-3"  dense elevation="4" rounded class="my-4 white--text" fixed></v-toolbar>
+<v-toolbar color="green darken-3"  dense elevation="4" rounded class="my-4 fixed-bar">
+  <v-switch class="white--text" dark v-model="$vuetify.theme.dark" hint="This toggles the global state of the Vuetify theme" inset label="Vuetify Theme Dark" persistent-hint></v-switch>
+  <v-text-field dark hide-details color="light-blue accent-3" prepend-icon="mdi-magnify" single-line></v-text-field></v-toolbar>
   <v-row justify="center">
 <v-card  v-for="element in projets" :key="element.id" class=" ma-4" max-width="344" >
   <v-col>
@@ -10,9 +11,9 @@
   <v-row>
   <v-card-title class="text-center text-break" style="height:150px;" >{{element["gmd:identificationInfo"]["gmd:MD_DataIdentification"]["gmd:citation"]["gmd:CI_Citation"]["gmd:title"]["gco:CharacterString"]["#text"]}}</v-card-title>
  <v-divider ></v-divider>
-  <p class="text-justify" v-snip:js="3" style="max-height:150px;margin-bottom:75px;margin-left:18px;margin-right:18px;margin-top:18px;">{{element["gmd:identificationInfo"]["gmd:MD_DataIdentification"]["gmd:abstract"]["gco:CharacterString"]["#text"]}}</p>
+  <p  class="mt-5" v-snip="{ lines: 5 }"  style="margin-bottom:46px;max-height:150px;word-wrap: break-word;margin-left:10px;margin-right:10px;">{{element["gmd:identificationInfo"]["gmd:MD_DataIdentification"]["gmd:abstract"]["gco:CharacterString"]["#text"]}}</p>
 
-  <v-btn color="green darken-3" class="white--text"  block style="position:absolute;bottom:0;margin-top:75px;">Consulter</v-btn>
+  <v-btn tile text color="green darken-3" class="white--text"  block style="position:absolute;bottom:0;">Consulter</v-btn>
 
   </v-row>
   </v-col>
@@ -21,6 +22,16 @@
   </v-row>
 </v-container>
 </template>
+
+
+<style scoped>
+.fixed-bar {
+  position: sticky;
+  position: -webkit-sticky; /* for Safari */
+  top: 12px;
+  z-index: 2;
+}
+</style>
 
 <script>
 import axios from 'axios'
