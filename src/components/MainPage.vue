fdf88dcd880a8e69c6465cbaaec21b5e5563547f
ZITOUNI Clement <345567@cus.fr>
2022-07-18 11:47:47 +0200

    18/07

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -5,16 +5,18 @@
 
 <v-container>
 <v-row justify="center">
-<v-card  v-for="element in orderProjets" :key="element['gmd:fileIdentifier']['gco:CharacterString']['#text']" class="  ma-4" max-width="344" >
-  <v-col>
+<v-card   v-for="element in orderProjets" :key="element['gmd:identificationInfo']['gmd:MD_DataIdentification']['gmd:citation']['gmd:CI_Citation']['gmd:title']['gco:CharacterString']['#text']" class="  ma-4" max-width="344" >
+  <v-col >
+<div v-if="element['gmd:identificationInfo']['gmd:MD_DataIdentification'] !== undefined">
   <v-row v-if="element['gmd:identificationInfo']['gmd:MD_DataIdentification']['gmd:graphicOverview'] !== undefined">
     <v-img :aspect-ratio="16/9"    :src ="element['gmd:identificationInfo']['gmd:MD_DataIdentification']['gmd:graphicOverview']['gmd:MD_BrowseGraphic']['gmd:fileName']['gco:CharacterString']['#text']"></v-img>
   </v-row>
-  <v-row v-else>
+    <v-row v-else>
     <v-img :aspect-ratio="16/9"    :src ="'https://cdn.vuetifyjs.com/images/parallax/material.jpg'"></v-img>
   </v-row>
-  <v-row>
-    <v-card-title  class="text-center text-break"  v-snip="{ lines: 4}" style="height:150px;" >{{element["gmd:identificationInfo"]["gmd:MD_DataIdentification"]["gmd:citation"]["gmd:CI_Citation"]["gmd:title"]["gco:CharacterString"]["#text"]}}</v-card-title>
+  </div>
+  <v-row >
+    <v-card-title  class="text-center text-break"  v-snip="{ lines: 4}" style="height:150px;" >{{element['gmd:identificationInfo']['gmd:MD_DataIdentification']['gmd:citation']['gmd:CI_Citation']['gmd:title']['gco:CharacterString']['#text']}}</v-card-title>
     <v-divider ></v-divider>
     <p  class="mt-5" v-snip="{ lines: 5 }"  style="margin-bottom:46px;max-height:150px;word-wrap: break-word;margin-left:10px;margin-right:10px;">{{element["gmd:identificationInfo"]["gmd:MD_DataIdentification"]["gmd:abstract"]["gco:CharacterString"]["#text"]}}</p>
     <v-btn @click="$router.push({ path: element['gmd:fileIdentifier']['gco:CharacterString']['#text'] })" tile text color="green darken-3" class="white--text"  block style="position:absolute;bottom:0;">Consulter</v-btn>
@@ -30,6 +32,7 @@
 
 
 <script>
+
 import axios from 'axios'
 import AppBar from './AppBar'
 
@@ -51,16 +54,23 @@ export default{
 
 methods:{
   getInitialResults(){
+    
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
     axios
     .get('https://wmsiglw.cus.fr/geonetwork/srv/api/sitemap')
     .then(response => (this.info = convert.xml2js(response.data)))
     .then(() => this.info= this.info.elements[0].elements)
     .then(() => this.info.forEach((element) => {this.liste.push(element.elements[0].elements[0].text.split('/')[7].split('?')[0]);}))
-    .then(() => this.liste.forEach((element)  =>  axios.get('https://wmsiglw.cus.fr/geonetwork/srv/api/records/' + element + '/formatters/json'   ).then(response => (this.projets.push(response.data)))))
+    .then(() => this.liste.slice(0,this.pos).forEach((element)  =>  axios.get('https://wmsiglw.cus.fr/geonetwork/srv/api/records/' + element + '/formatters/json'   ).then(response => (this.projets.push(response.data)))))
 
   },
   getNextResults(){
+const observer = new IntersectionObserver(entries => {
+  const firstEntry = entries[0];
+  if (firstEntry.isIntersecting) {
+    // Handle intersection here...
+  }
+});
     window.onscroll = () =>{
       let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
       if(bottomOfWindow && this.pos < this.liste.length){
@@ -75,7 +85,7 @@ methods:{
   
 computed: {
   orderProjets: function () {
-     return [...this.projets].sort( (a, b) => a['gmd:fileIdentifier']['gco:CharacterString']['#text'].localeCompare(b['gmd:fileIdentifier']['gco:CharacterString']['#text']))
+     return [...this.projets].sort( (a, b) => a['gmd:identificationInfo']['gmd:MD_DataIdentification']['gmd:citation']['gmd:CI_Citation']['gmd:title']['gco:CharacterString']['#text'].localeCompare(b['gmd:identificationInfo']['gmd:MD_DataIdentification']['gmd:citation']['gmd:CI_Citation']['gmd:title']['gco:CharacterString']['#text']))
   },
 },
 components: {
@@ -88,8 +98,7 @@ components: {
 
   mounted () {
 
-    this.getNextResults();
-  
+    this.getNextResults()
 
   },
 
